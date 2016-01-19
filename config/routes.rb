9eec9ee6294d6b75754dc36f88236d4132f4e6cb
Julien Chantereau <jchantereau@gmail.com>
2016-01-19 15:50:10 +0100

    booking-flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  resources :sessions, only: [:index, :show]
+  resources :sessions, only: [:index, :show] do
+    resources :bookings, only: [:new, :create]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
