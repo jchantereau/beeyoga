910429b8aeae8f08c434c6f999e203a1125da593
Julien Chantereau <jchantereau@gmail.com>
2016-01-25 17:04:30 +0100

    booking creation ok, no confirmation flow yet

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,8 +8,9 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(booking_params)
     @booking.validated = false
+    @booking.session_id = @session.id
     if @booking.save
-      raise
+      flash.notice = "Votre demande d'inscription a bien été reçu, nous revenons vers vous dans les plus bref délais"
       redirect_to sessions_path
     else
       flash[:alert] = "Votre réservation est incomplète, merci de vérifier que tous les champs sont remplis"
