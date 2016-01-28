eedfc623b6a6794eb79c523bbd63758d813c1282
Julien Chantereau <jchantereau@gmail.com>
2016-01-28 16:25:51 +0100

    Stripe payment working

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'figaro'
 gem 'jbuilder', '~> 2.0'
 gem 'devise'
 gem 'money-rails'
+gem 'stripe'
 
 gem 'sass-rails', '~> 5.0'
 gem 'jquery-rails'
---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,38 @@
+class PaymentsController < ApplicationController
+  before_action :set_booking
+
+  def new
+  end
+
+  def create
+    @amount = @booking.amount_cents
+
+    customer = Stripe::Customer.create(
+        source: params[:stripeToken],
+        email: params[:stripeEmail]
+      )
+      # You should store this customer id and re-use it.
+
+      charge = Stripe::Charge.create(
+        customer: customer.id,
+        amount:       @amount,  # in cents
+        description:  "Payment for session #{@booking.session_sku} for booking #{@booking.id}",
+        currency:     'eur'
+      )
+
+      @booking.update(payment: charge.to_json, state: 'paid')
+      flash.notice = "Success"
+
+      redirect_to root_path
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_booking_payment_path(@booking)
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.where(state: 'pending').find(params[:booking_id])
+  end
+end
