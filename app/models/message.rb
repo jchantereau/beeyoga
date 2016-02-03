5a35fcb42e83ab345e0406c927c811282db2bd19
Julien Chantereau <jchantereau@gmail.com>
2016-02-03 14:40:23 +0100

    admin mailer for message

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,11 +2,15 @@ class Message < ActiveRecord::Base
   validates_presence_of :name, :email, :content
   validates_format_of :email, :with => /@/
 
-  # after_create: send_confirmation_email
+  after_create :send_confirmation_email
 
   private
 
   def send_confirmation_email
+    MessageMailer.message_confirmation(self).deliver_now
+  end
 
+  def send_admin_email
+    MessageMailer.message_admin(self).deliver_now
   end
 end
